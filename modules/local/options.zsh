bdcbd97011061c84749ae93429e5e486f9094112
Zhiming Wang <zmwangx@gmail.com>
2015-07-20 23:02:37 -0700

    local module: enable confirmation for long completion listing
    Disable scrolling in completion listings. That sucks.

--- modules/local/options.zsh
+++ modules/local/options.zsh
@@ -4,3 +4,9 @@ unsetopt bang_hist
 unsetopt path_dirs
 
 export PROMPT_EOL_MARK="" # PROMPT_SP
+export LISTMAX=0 # ask if completion listing can't fit in one screen
+
+# disable scrolling in completion listings
+# see list-prompt in Completion-System.html, and zsh/complist in Zsh-Modules.html
+zstyle -d ':completion:*:default' list-prompt
+unset LISTPROMPT
