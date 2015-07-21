941f932649befde4aad374ee3ee5389ce940599c
Zhiming Wang <zmwangx@gmail.com>
2015-07-20 23:25:06 -0700

    local: load run-help and replace the lame alias to man

--- modules/local/init.zsh
+++ modules/local/init.zsh
@@ -5,6 +5,7 @@
 #   Zhiming Wang <zmwangx@gmail.com>
 #
 
+source "${0:h}/load.zsh"
 source "${0:h}/options.zsh"
 source "${0:h}/widgets.zsh"
 source "${0:h}/keybindings.zsh"
---
+++ modules/local/load.zsh
@@ -0,0 +1,3 @@
+# use zsh/Functions/Misc/run-help rather than the lame alias to man
+unalias run-help
+autoload -Uz run-help
