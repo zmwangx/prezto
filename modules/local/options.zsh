22e440252e82b106e4e7a737084bfbb843bc703a
Zhiming Wang <zmwangx@gmail.com>
2015-09-21 01:22:49 -0700

    local module: turn off bracked paste mode

--- modules/local/options.zsh
+++ modules/local/options.zsh
@@ -6,6 +6,9 @@ unsetopt path_dirs
 export PROMPT_EOL_MARK="" # PROMPT_SP
 export LISTMAX=0 # ask if completion listing can't fit in one screen
 
+# turn off bracketed paste, which screws up auto-quoting of URL characters and messes up the Emacs shell
+unset zle_bracketed_paste
+
 # disable scrolling in completion listings
 # see list-prompt in Completion-System.html, and zsh/complist in Zsh-Modules.html
 zstyle -d ':completion:*:default' list-prompt
