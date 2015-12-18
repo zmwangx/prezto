e94b33bd222abb4dad93bd4e7b185f10909d8f44
Zhiming Wang <zmwangx@gmail.com>
2015-12-18 12:55:53 -0800

    Add .dir-locals.el for inserting authorship boilerplate
    Two boilerplate: one for Markdown, and one for anything else (anything
    else basically means zsh source code in Prezto).

---
+++ .dir-locals.el
@@ -0,0 +1,18 @@
+((nil .
+      ((auto-insert-alist .
+                          (("\.md\\'" nil
+                            ;; Markdown template
+                            _ \n
+                            \n
+                            "## Authors" \n
+                            "* [" user-full-name "](" (concat "https://github.com/"
+                                                              (car (split-string user-mail-address "@"))) ")" \n)
+                           ("" nil
+                            ;; Shell template
+                            "#" \n
+                            "# " _ \n
+                            "#" \n
+                            "# Authors:" \n
+                            "#   " user-full-name "<" user-mail-address ">" \n
+                            "#" \n
+                            \n))))))
