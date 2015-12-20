c04f20e5bc07d15e207a491d1604fed77514f99a
Zhiming Wang <zmwangx@gmail.com>
2015-12-20 11:55:53 -0800

    transmission/init.zsh: Be more liberal in accepting torrent specs
    Okay, I always thought transmission-remote only accepts comma-separated
    lists as torrent specs... But apparently I'm wrong. Not sure why I
    never seemed to have read the man page...
    
    Since a whole range of things could be torrent specs, the filtering
    __tms_filter_torrent_ids becomes pointless. Remove that part of the
    code.

--- modules/transmission/README.md
+++ modules/transmission/README.md
@@ -8,13 +8,18 @@ Shortcuts for working with `transmission-daemon` and `transmission-remote`.
 * `tmsd-stop` for stopping the daemon;
 * `tms` is alias for `transmission-remote`;
 * `tmsa [torrent|magnet]...` for adding torrents;
-* `tmsi id...` for retrieving torrent info;
-* `tmsf id...` for listing torrent files;
-* `tmse id fileno_list` for excluding files from torrent;
-* `tmsp id...` for pausing torrents;
-* `tmsr id...` for resuming torrents;
-* `tmsv id...` for verifying torrents;
-* `tms-remove id...` for removing transfers.
+* `tmsi spec...` for retrieving torrent info;
+* `tmsf spec...` for listing torrent files;
+* `tmse spec fileno_list` for excluding files from torrent;
+* `tmsp spec...` for pausing torrents;
+* `tmsr spec...` for resuming torrents;
+* `tmsv spec...` for verifying torrents;
+* `tms-remove spec...` for removing transfers.
+
+Here `spec` a torrent specification recognized by
+`transmission-remote`. Accepted torrent specs include, but may not be limited
+to, `all`, `active`, torrent hash, numeric ID or ID range, or comma-delimited
+list of specs.
 
 ## Authors
 * [Zhiming Wang](https://github.com/zmwangx)
--- modules/transmission/init.zsh
+++ modules/transmission/init.zsh
@@ -23,30 +23,13 @@ tmsa () {
     return $ret
 }
 
-# This helper scans argv for non-numeric arguments (more precisely, non-
-# nonnegative integer arguments) and print an error for those
-# arguments. Afterwards it set the array argument `torrents' to the list of
-# (potentially) valid torrent IDs.
-#
-# Note that the caller is required to declare local arguments `torrents' and
-# `ret', where `torrents' is an array and `ret' is a scalar for return value.
-__tms_filter_torrent_ids () {
-    local invalid_args
-    invalid_args=( ${@:#<->} )
-    [[ -z $invalid_args ]] || {
-        print_error "The following arguments are not numeric torrent IDs: ${(j:, :)${(qq)invalid_args[@]}}.";
-        ret=1;
-    }
-    set -A torrents ${(M)@:#<->}
-}
-
 # A central dispatcher to fight code duplication.
 #
 # Arguments are command name (tmsi, tmsf, etc.) plus torrent IDs.
 __tms_central_command () {
-    local command ret=0 torrents
+    local command torrents
     command=$1 && shift
-    __tms_filter_torrent_ids $@
+    torrents=$@
     [[ -n $torrents ]] || { print_error "No torrents specified."; return 1; }
 
     local message_opening action_opt
@@ -101,11 +84,10 @@ tmse () {
         cat >&2 <<HELP
 Usage: tmse <torrent> <filelist>
 
-<torrent> is the numeric torrent ID, <filelist> is a comma-delimited list of file No.s.
+<torrent> is a torrent spec, <filelist> is a comma-delimited list of file No.s.
 HELP
         return 1
     fi
-    [[ $1 == <-> ]] || { print_error "'$1' is not a numeric torrent ID."; return 1; }
     command transmission-remote --torrent $1 --no-get $2 \
         || { print_error "Failed to exclude files '$2' from torrent $1."; return 1; }
 }
