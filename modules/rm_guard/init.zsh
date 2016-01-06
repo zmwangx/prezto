332c3cae540a660a3ee05e7b1a2302fe37183c38
Zhiming Wang <zmwangx@gmail.com>
2016-01-05 21:44:43 -0800

    rm_guard/init.zsh: Implement a no-fly list feature
    Learned it the hard way. I accidentally removed part of ~/.local/share,
    which unfortunately is not a git repo.

--- modules/rm_guard/init.zsh
+++ modules/rm_guard/init.zsh
@@ -1,6 +1,10 @@
 #
-# Functional wrapper for rm that blocks removals of git repos with the -f or
+# Functional wrapper for rm that blocks removals of git repos without the -f or
 # --force option.
+#
+# It also downright reject the removal of several specially protected
+# directories, currently: $HOME/.config, $HOME/.local and $HOME/.local/share,
+# whether -f is specified or not. To remove those, use command rm instead.
 #
 # Authors:
 #   Zhiming Wang <zmwangx@gmail.com>
@@ -9,6 +13,26 @@
 rm () {
     setopt localoptions noshwordsplit noksharrays
     local args_backup force node
+
+    # Resolve path with pure Zsh, but do not resolve links.
+    _rm_guard_resolve_path () {
+        setopt localoptions extendedglob
+        local node match MATCH MBEGIN MEND
+        node=$1
+        [[ $node != /* ]] && node=$PWD/$node
+        node=${node//\/##/\/}
+        while [[ $node == */./* ]]; do
+            node=${node/\/.\//\/}
+        done
+        node=${node/%\/./\/}
+        while [[ $node == */[^/]##/../* ]]; do
+            print $node
+            node=${node/\/[^\/]##\/..\//\/}
+        done
+        node=${node/%\/[^\/]##\/../\/}
+        print -R $node
+    }
+
     set -A args_backup $@
     while :; do
         case $1 in
@@ -22,6 +46,12 @@ rm () {
         # -f, --force hasn't been specified && node exists && node is not a symlink && node is a git repo
         [[ -z $force && -e $node && ! -h $node && ( -e $node/.git || $node == (.git|*/.git) ) ]] && {
             printf "\e[31m'%s' is a git repo -- won't remove without the -f or --force option\e[0m\n" $node
+            return 1
+        }
+        # compare to no-fly list
+        node=$(_rm_guard_resolve_path $node)
+        [[ $node == ($HOME/.config|$HOME/.local|$HOME/.local/share) ]] && {
+            printf "\e[31;1mREMOVING '%s' IS DANGEROUS! No, for your safety we won't let you do that.\e[0m\n" $node
             return 1
         }
     done
